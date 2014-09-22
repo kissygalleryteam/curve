17b18a1c3635a8bd5d715a5a33af06ac06b8cb04
弘树 <tiehang.lth@alibaba-inc.com>
2014-09-22 20:52:02 +0800

    sync daily branch 1.0.1

--- 1.0.0/build/mods-min.js
+++
@@ -1,1 +0,0 @@
-KISSY.config("modules",{"kg/curve/1.0.0/index":{requires:["node","base","./lib/util"]}});
--- doc/build/mods-min.js
+++
@@ -1,1 +0,0 @@
-KISSY.config("modules",{"kg/curve/1.0.1/index":{requires:["node","base","./lib/util"]}});
